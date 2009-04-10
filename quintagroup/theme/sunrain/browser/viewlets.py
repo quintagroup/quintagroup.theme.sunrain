d8dede60a0053976e6a5b29204119b33d4ef9eda
gvizdyk <gvizdyk@quintagroup.com>
2009-04-10 15:52:13 +0000

    editing styles and registry new viewlets

--- quintagroup/theme/sunrain/browser/viewlets.py
+++
@@ -1,6 +0,0 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets import common
-from plone.app.layout.viewlets import content
-
-class GlobalSectionsViewlet(common.GlobalSectionsViewlet):
-    render = ViewPageTemplateFile('templates/global_sections.pt')
