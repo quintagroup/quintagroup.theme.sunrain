211324de8786f94bbfd80982644f7aea2a0d88f8
naivok <naivok@quintagroup.com>
2011-11-22 11:58:36 +0000

    drop mobile.css

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '6.0.1'
+version = '6.0.2'
 
 tests_require=['zope.testing']
 
