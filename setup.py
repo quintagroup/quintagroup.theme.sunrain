31659c8cd8ca38ad3595e79b4435e9f5b7d09c2f
naivok <naivok@quintagroup.com>
2012-09-05 11:23:00 +0000

    Fixed style to history

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of quintagroup.theme.sunrain
 import os
 from setuptools import setup, find_packages
 
-version = '6.0.9'
+version = '6.0.10'
 
 setup(name='quintagroup.theme.sunrain',
       version=version,
